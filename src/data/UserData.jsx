c499a76e45eacc5ffa6b009f4947134af7f96f91
Faizan Hamid Lone <90542362+saffronhamid@users.noreply.github.com>
2024-12-02 14:24:17 +0100

    updated

---
+++ src/data/UserData.jsx
@@ -0,0 +1,42 @@
+export const UserData = {
+  name: "Faizan Hamid",
+  about : "I am a self-taught front-end developer with a proven expertise in crafting dynamic, universally accessible websites optimized for seamless performance across all devices. My forte lies in engineering innovative solutions that elevate user experiences to exceptional standards. With proficiency in React.js, Redux, JavaScript, HTML5, and CSS, I deliver robust and scalable code. My adeptness with Git version control ensures meticulous code management and effective collaboration. Additionally, I excel in developing sophisticated applications using Mini Program Studio frameworks, consistently surpassing client expectations with high-quality, tailored solutions",
+  resumeUrl : "",
+  FooterLink : "https://www.linkedin.com/in/faizanhamid/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/saffronhamid",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/saffronhamid/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "https://twitter.com/saffronhamid",
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "https://instagram.com/saffronhamid",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "React Js Developer",
+      "NodeJs Developer",
+      "Javascript Developer",
+      "Data Science",
+      "Machine Learning",
+      "Generative AI"
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
